89ef7005ad98a48cd76c794532ff2aaac12b0786
Agith-Ravn <76996657+Agith-Ravn@users.noreply.github.com>
2021-02-10 14:17:21 +0100

    10.12.2021

---
+++ StartIT_Uke_6_Obligatorisk_Oppgave_2/Controller.js
@@ -0,0 +1,58 @@
+/* -------------------------------------------------------| C O N T R O L L E R |----------------------------------------------------------------*/
+function velgDiagram(valgtDiagram) {
+    //console.log(valgtDiagram) //henter diagramNr
+    chosenBarValue = numbers[valgtDiagram - 1] //valgt diagram verdi til senere bruk
+
+    if (chosenBar == valgtDiagram) { //hvis chosenBar + valgtDiagram har samme verdi = string byttes til "ingen"
+        chosenBar = 'ingen';
+        knapp = "disabled"; //gjør det ikke mulig å slette eller endre når du ikke har valgt en diagram.
+        knapp2 = ""; //gjør det mulig å legge til nye diagram
+
+        chosenBarValue = ''; // må ha denne slik at chosenBarValue blir '', når du deselecter
+        inputValue = "0" // må ha denne, ellers vil input verdi være det samme som verdi du endret stolpe
+    } else {
+        chosenBar = valgtDiagram; //velger diagram hvis du ikke har valgt den samme verdien
+        knapp = ""; //gjør det mulig å endre og slette når du har valgt diagram
+        knapp2 = "disabled"; //ikke mulig å lage ny når du har valgt diagram
+    }
+    show();
+}
+
+
+function slettDiagram() {
+    let posisjon = chosenBar; //valgt diagram (nr1-8) > går inn i posisjon 
+    posisjon -= 1 ; //chosenBar starter på 1, så du må fjerne 1, for at posisjon verdi for at dette skal stemme når du bruker splice
+    //console.log(posisjon)
+    numbers.splice(posisjon, 1); //fjerner array element fra posisjon (1-8)verdi 
+    chosenBar = "ingen"; 
+    knapp = "disabled"
+    knapp2 = "";
+    show();
+}
+
+
+function endreDiagram() {   
+    if (inputValue < 1 || inputValue >= 11) {
+        alert("Ugyldig verdi. Verdi skal være mellom 1 og 10..");
+    }
+    else if (inputValue == "ingen" || inputValue == chosenBarValue) {alert("Du har valgt samme verdi")}
+    else {
+        numbers[chosenBar - 1] = inputValue;
+        chosenBarValue = inputValue;
+    }    
+    show();
+}
+
+
+function leggTilDiagram() {
+    if (inputValue == 0)
+        alert ("Du må legge til en verdi")
+
+    else if (inputValue < 1 || inputValue >= 11) {
+        alert("Ugyldig verdi. Verdi skal være mellom 1 og 10..");        
+    } 
+    else {
+        numbers.length >= 8 ? alert("Du kan ikke ha flere enn 8 stolpe diagrammer") : numbers.push(inputValue);
+    }
+    show();
+}
