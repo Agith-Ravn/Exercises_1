8ab2bc775e7dc9405a71c49e593c80dd9d65d9f8
Agith-Ravn <76996657+Agith-Ravn@users.noreply.github.com>
2021-05-12 16:28:17 +0200

    .

---
+++ M3/Oppgave2/Oppgave2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Oppgave2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Hvis isValid er lik false -> Start ShowHelpText()
+            if (!IsValid(args)) 
+            {
+                ShowHelpText();
+                return;
+            }
+            var length = Convert.ToInt32(args[0]);
+        }
+
+        private static bool IsValid(string[] args)
+        {
+            if (args.Length != 2) return false;
+
+            var lengthString = args[0];
+            var options = args[1];
+
+            foreach (var character in lengthString)
+            {
+                if (!char.IsDigit(character))
+                {
+                    return false;
+                }
+            }
+
+            foreach (var character in options)
+            {
+                if (character != 'l'
+                    && character != 'L'
+                    && character != 'd'
+                    && character != 's')
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static void ShowHelpText()
+        {
+
+            Console.WriteLine("PasswordGenerator <length> <options>");
+            Console.WriteLine(" Options:");
+            Console.WriteLine(" - l = lower case letter");
+            Console.WriteLine(" - L = upper case letter");
+            Console.WriteLine(" - d = digit");
+            Console.WriteLine(" - s = special character (!\"#¤%&/(){}[]");
+            Console.WriteLine("Example: PasswordGenerator 14 lLssdd");
+            Console.WriteLine("         Min. 1 lower case");
+            Console.WriteLine("         Min. 1 upper case");
+            Console.WriteLine("         Min. 2 special characters");
+            Console.WriteLine("         Min. 2 digits");
+        }
+    }
+}
