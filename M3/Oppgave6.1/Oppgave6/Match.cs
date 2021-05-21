71856a45e58432b4ee76234ca9c537f4834b1189
Agith-Ravn <76996657+Agith-Ravn@users.noreply.github.com>
2021-05-21 13:48:46 +0200

    Oppgave6

--- M3/Oppgave6.1/Oppgave6/Match.cs
+++ M3/Oppgave6.1/Oppgave6/Match.cs
@@ -2,15 +2,59 @@
 {
     public class Match
     {
-        /* 0. Kommando kanskje?
-         * 1. Class 
-         * 2. Add Goal
-         * 3. Check if bet is correct
-         * 4. Stop
-         * 5. Hvorfor get score????
-         * 
-         *
-         *
+        /* 1. Kommando kanskje?
+         * 2. Class 
+         * 3. Add Goal
+         * 4. Check if bet is correct
+         * 5. Stop
+         * 6. Hvorfor get score????
          */
+
+
+        private int _homeGoals;
+        private int _awayGoals;
+        private readonly string _bet;
+
+        public bool IsRunning { get; private set; }
+
+        public Match(string bet)
+        {
+            _bet = bet;
+            IsRunning = true;
+        }
+
+        public void AddGoal(bool isHomeTeam)
+        {
+            if (isHomeTeam) _homeGoals++;
+            else _awayGoals++;
+        }
+
+        public bool IsBetCorrect()
+        {
+            /* Nested ternary Operator = a ? b : c ? d : e
+             * a ? b : (c ? d : e)
+             * Hvis a er true = b
+             * hvis a er false -> sjekk c ? d : e -> hvis c er true = d
+             *                                       hvisc er false = e
+             *                                       
+             * Hvis homeGoals er lik awayGoals = "U"
+             * Hvis homeGoals IKKE er lik awayGoals -> sjekk om homeGoals er større enn awayGoals = "H"
+             *                                             hvis homeGoals er mindre enn awayGoals = "B"
+             */
+
+            var result = _homeGoals == _awayGoals ? "U" : _homeGoals > _awayGoals ? "H" : "B";
+            return _bet.Contains(result);
+        }
+
+        public void Stop()
+        {
+            IsRunning = false;
+        }
+
+        public string GetScore()
+        {
+            //Returnerer variablene med verdier som string
+            return _homeGoals + "-" + _awayGoals;
+        }
     }
 }
--- M3/Oppgave6.1/Oppgave6/Program.cs
+++ M3/Oppgave6.1/Oppgave6/Program.cs
@@ -6,57 +6,33 @@ namespace Oppgave6
     {
         static void Main(string[] args)
         {
-            //Output tekst (Info om tipping)
             Console.Write("Gyldig tips: \r\n - H, U, B\r\n - halvgardering: HU, HB, UB\r\n - helgardering: HUB\r\nHva har du tippet for denne kampen? ");
-
-            //Input går til bet
+            
             var bet = Console.ReadLine();
+            var match = new Match(bet);
 
-            //Start verdier
-            var homeGoals = 0;
-            var awayGoals = 0;
-            var matchIsRunning = true;
-
-            //Mens matchIsRunning er true
-            while (matchIsRunning)
+            while (match.IsRunning)
             {
-                //Output tekst (Info om kommandoer)
                 Console.Write("Kommandoer: \r\n - H = scoring hjemmelag\r\n - B = scoring bortelag\r\n - X = kampen er ferdig\r\nAngi kommando: ");
                 
                 //Input kommando
                 var command = Console.ReadLine();
 
-                //????
-                matchIsRunning = command != "X";
+                //Stoppe match, hvis du skriver X
+                if (command == "X") match.Stop();
 
-                //Hvis input kommando er lik H -> homeGoals får +1
-                if (command == "H") homeGoals++;
+                //Hvis du skriver H > true > homeGoals++
+                //Hvis du skriver B > false > awayGoals++
+                if (command == "H") match.AddGoal(true);
+                else if (command == "B") match.AddGoal(false);
 
-                //Hvis IKKE input kommando er lik B -> awayGoals får +1
-                else if (command == "B") awayGoals++;
-
-                //
-                Console.WriteLine($"Stillingen er {homeGoals}-{awayGoals}.");
+                //Må bruker ${} med match.GetScore() inni. GetScore returnerer string
+                Console.WriteLine($"Stillingen er {match.GetScore()}.");
             }
 
-            /* Nested ternary Operator = a ? b : c ? d : e
-             * a ? b : (c ? d : e)
-             * Hvis a er true = b
-             * hvis a er false -> sjekk c ? d : e -> hvis c er true = d
-             *                                       hvisc er false = e
-             *                                       
-             * Hvis homeGoals er lik awayGoals = "U"
-             * Hvis homeGoals IKKE er lik awayGoals -> sjekk om homeGoals er større enn awayGoals = "H"
-             *                                             hvis homeGoals er mindre enn awayGoals = "B"
-             */
-            var result = homeGoals == awayGoals ? "U" : homeGoals > awayGoals ? "H" : "B";
-
             //Sjekker om det du tippet inneholder i resultater -> sender true eller false
-            var isBetCorrect = bet.Contains(result);
-
-            //Hvis isBetCorrect er true = riktig, hvis ikke = feil
-            var isBetCorrectText = isBetCorrect ? "riktig" : "feil";
-
+            string isBetCorrectText = match.IsBetCorrect() ? "riktig" : "feil";
+            
             //Output tekst om hva du har tippet
             Console.WriteLine($"Du tippet {isBetCorrectText}");
         }
