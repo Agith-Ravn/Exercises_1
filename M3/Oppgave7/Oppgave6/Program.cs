40ef630ecd4704f83cfb1dfe1ff4994d655bd428
Agith-Ravn <76996657+Agith-Ravn@users.noreply.github.com>
2021-05-23 13:48:39 +0200

    .

---
+++ M3/Oppgave7/Oppgave6/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Oppgave6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Output text
+            Console.Write("Gyldig tips: \r\n - H, U, B\r\n - halvgardering: HU, HB, UB\r\n - helgardering: HUB\r\nSkriv inn dine 12 tips med komma mellom: ");
+            
+            //Input text
+            var betsText = Console.ReadLine();
+
+            //Gjør om class til object -> Sendeer input tekst -> Splitter opp -> legger i array
+            var matches = new TwelveMatches(betsText);
+
+            //Kjøres til det finnes en break;
+            while (true)
+            {
+                //Output text
+                Console.Write("Skriv kampnr. 1-12 for scoring eller X for alle kampene er ferdige\r\nAngi kommando: ");
+                
+                //Input -> Legger inn i command
+                var command = Console.ReadLine();
+
+                //Hvis input er X -> Stop
+                if (command == "X") break;
+
+                //Konverter kommando til nummer?
+                var matchNo = Convert.ToInt32(command);
+
+                //Output text 
+                Console.Write($"Scoring i kamp {matchNo}. \r\nSkriv H for hjemmelag eller B for bortelag: ");
+                
+                //Input text -> legger inn i team
+                var team = Console.ReadLine();
+
+                //Setter i gang AddGoal() method fra TwelveMatches class
+                matches.AddGoal(matchNo, team == "H");
+                
+                //Hviser om du har gjettet riktig eller ikke
+                matches.ShowAllScores();
+
+                //Resultat
+                matches.ShowCorrectCount();
+
+            }
+        }
+    }
+}
---
+++ M3/Oppgave7/Oppgave6/TwelveMatches.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Oppgave6
+{
+    public class TwelveMatches
+    {
+        //Deklarerer en tom pricate array til senere bruk
+        private Match[] _matches;
+
+        public TwelveMatches(string betsText)
+        {
+            //Deler opp string ved komma og legger inn i bets som array
+            var bets = betsText.Split(',');
+
+            //Lager en array med 12 elements, men er tomme..
+            _matches = new Match[12];
+
+            //For loop og legger inn hva du tipper i _matches array
+            for (var i = 0; i < 12; i++)
+            {
+                _matches[i] = new Match(bets[i]);
+            }
+        }
+
+
+        public void AddGoal(int matchNo, bool isHomeTeam)
+        {
+            var selectedIndex = matchNo - 1;
+            var selectedMatch = _matches[selectedIndex];
+            //selectedMatch.AddGoal(isHomeTeam); - Hvorfor er denne her?
+        }
+
+
+        public void ShowAllScores()
+        {
+            //For loop igjennom matches
+            for (var index = 0; index < _matches.Length; index++)
+            {
+                //match[1], match[2] osv inni match
+                var match = _matches[index];
+
+                //Hente matchnr fra index (pga den starter på 0)
+                var matchNo = index + 1;
+
+                //Sjekke om hvem som har vunnet + og du har tippet riktig
+                var isBetCorrect = match.IsBetCorrect();
+                var isBetCorrectText = isBetCorrect ? "riktig" : "feil";
+
+                //Output 
+                Console.WriteLine($"Kamp {matchNo}: {match.GetScore()} - {isBetCorrectText}");
+            }
+        }
+
+        public void ShowCorrectCount()
+        {
+            var correctCount = 0;
+            foreach (var match in _matches)
+            {
+                //Hvis du har tippet riktig -> legg til i correctCount
+                if (match.IsBetCorrect()) correctCount++;
+            }
+            Console.WriteLine($"Du har {correctCount} rette.");
+        }
+    }
+}
