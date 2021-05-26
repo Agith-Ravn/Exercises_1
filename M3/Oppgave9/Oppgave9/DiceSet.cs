b0ee5168896d47ccd9d81690bf094cc5db4a9ead
Agith-Ravn <76996657+Agith-Ravn@users.noreply.github.com>
2021-05-26 22:26:31 +0200

    .

--- M3/Oppgave9/Oppgave9/DiceSet.cs
+++ M3/Oppgave9/Oppgave9/DiceSet.cs
@@ -2,6 +2,13 @@
 {
     public class DiceSet
     {
+        //privat tom array av Dice (class) som heter _dice
         private Dice[] _dice;
+
+
+        public int[] GetValues()
+        {
+
+        }
     }
 }
