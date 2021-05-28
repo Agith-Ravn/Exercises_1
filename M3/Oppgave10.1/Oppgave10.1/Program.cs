f6bd71c647e8b44f667af102e5ef66d22bbe6481
Agith-Ravn <76996657+Agith-Ravn@users.noreply.github.com>
2021-05-28 22:53:25 +0200

    .

---
+++ M3/Oppgave10.1/Oppgave10.1/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Linq;
+
+namespace Oppgave10._1
+{
+    class Program
+    {
+
+        static readonly Random random = new Random();
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PassordGeneratorInfo());
+            string[] input = Console.ReadLine().Split(' ');
+
+            ////Kun tii testing
+            //for (int j = 0; j < input.Length; j++)
+            //{
+            //    Console.WriteLine(input[j]);
+            //}
+
+            //Hvis isValid returnerer false -> Stop
+            if (!IsValid(input))
+            {
+                UgyldigInput();
+                return; 
+            }
+
+            var passwordLength2 = Convert.ToInt32(input[0]);
+            var passwordContent2 = input[1];
+
+            string password = null;
+
+            while (passwordContent2.Length > 0)
+            {
+                foreach (var character in passwordContent2)
+                {
+                    if (character == 'l')
+                    {
+                        password += GetRandomLowerCaseLetter();
+                    }
+
+                    if (character == 'L')
+                    {
+                        password += GetRandomUpperCaseLetter();
+                    }
+
+                    if (character == 'd')
+                    {
+                        password += RandomDigit();
+                    }
+
+                    if (character == 's')
+                    {
+                        password += GetRandomSpecialChar();
+                    }
+
+
+
+                }
+
+               Console.WriteLine(password);
+               break;
+            }
+        }
+
+        public static string GetRandomSpecialChar()
+        {
+            const string chars = "!\"#¤%&/(){}[]";
+            int index = random.Next(0, chars.Length);
+            return chars[index].ToString();
+
+        }
+
+        private static string GetRandomLowerCaseLetter()
+        {
+            return GetRandomLetter('a', 'z').ToString(); ;
+        }
+
+        private static string GetRandomUpperCaseLetter()
+        {
+            return GetRandomLetter('A', 'Z').ToString(); ;
+        }
+
+        static char GetRandomLetter(char min, char max)
+        {
+            return (char)random.Next(min, max);
+        }
+
+        private static string RandomDigit()
+        {
+            return random.Next(5, 15).ToString();
+        }
+
+        public static bool IsValid(string[] input)
+        {
+            if (input.Length != 2) return false;
+
+            var passwordLength2 = input[0];
+            var passwordContent2 = input[1];
+
+            foreach (var character in passwordLength2)
+            {
+                if (!char.IsDigit(character)) return false;
+            }
+
+            foreach (var character in passwordContent2)
+            {
+                const string validCharacters = "lLds";
+                if (!validCharacters.Contains(character)) return false;
+                //if (character != 'l' || character != 'L' || character != 'd' || character != 's') return false;
+            }
+
+            return true;
+        }
+
+        private static string PassordGeneratorInfo()
+        {
+            string info = "PasswordGenerator \n  Options: \n  - l = lower case letter " +
+                   "\n  - L = upper case letter \n  - d = digit \n  - s = special character(!\"#¤%&/(){}[]" +
+                   "\nExample: PasswordGenerator 14 lLssdd \nMin. 1 lower case " +
+                   "\nMin. 1 upper case \nMin. 2 special characters \nMin. 2 digits\n\n";
+            
+            return info;
+        }
+
+        private static void UgyldigInput()
+        {
+            Console.WriteLine("Ugylding kommando.. Prøv igjen:");
+            Console.WriteLine(PassordGeneratorInfo());
+        }
+    }
+}
---
+++ M3/Oppgave10/Oppgave10/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Oppgave10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int range med 250
+            var range = 250;
+
+            int total = 0;
+            int total2 = 0;
+
+            //Tom array som kalles for counts med 250 plasser
+            //Kunne jo bare skrevet "var counts = new int[250];"?
+            var counts = new int[range];
+
+            //string som heter text med "something" inni
+            string text = "something";
+
+            //Mens IKKE string inneholder null eller mellromrom = true. 
+            //Altså alt under kjøres til while loop er stoppet (false)
+            //Console.WriteLine(!string.IsNullOrWhiteSpace(text));
+            while (!string.IsNullOrWhiteSpace(text))
+            {
+                //Venter på tekst
+                text = Console.ReadLine();
+
+                //double percentage = 100 * (double)counts[i] / total;
+
+                //Går igjennom text > putter hver bokstav i character, 
+                foreach (var character in text.ToUpper())
+                {
+                    //(int)character) = Konverterer char til int - Unicode
+                    //(counts[(int)character]) = Finner plassering av unicode, der du plusser +1 inni arrayElement
+                    counts[(int)character]++;
+                }
+
+                for (int i = 0; i < counts.Length; i++)
+                {
+                    total2 += counts[i];
+                }
+               
+                total = total2;
+                total2 = 0;
+                //Console.WriteLine(total);
+
+                //For loop 250 ganger
+                for (var i = 0; i < range; i++)
+                {
+                    //Hvis counts[1], counts[2] osv (opp til 250) er større enn 0
+                    if (counts[i] > 0)
+                    {
+                        //gjør int til char og legg inni character
+                        var character = (char)i;
+
+                        //Må gjøres om til double
+                        var percentage = 100 * (double)counts[i] / total;
+
+                        //Skriver ut bokstav (unicode) + antall av hver bokstav
+                        var output = character + " - " + percentage.ToString("F2") + "%";
+                        Console.CursorLeft = Console.BufferWidth - output.Length - 1;
+                        Console.WriteLine(output);
+                    }
+                }
+            }
+        }
+    }
+}
