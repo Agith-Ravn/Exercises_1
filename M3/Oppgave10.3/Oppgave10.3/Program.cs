1b3db610bb1ab70c514e378b4fc0f93fe59617fb
Agith-Ravn <76996657+Agith-Ravn@users.noreply.github.com>
2021-05-30 19:23:58 +0200

    .

--- M3/Oppgave10.3/Oppgave10.3/Program.cs
+++ M3/Oppgave10.3/Oppgave10.3/Program.cs
@@ -7,8 +7,8 @@ namespace Oppgave10._3
     {
         static void Main(string[] args)
         {
-            var names = new List<string> {"Agith", "Ida", "Kayla", "Bob"};
-            
+            var names = new List<string> { "Ida", "Kayla", "Bob", "Agith" };
+
             names.Add("Leo");
             names.Remove("Bob");
 
@@ -18,13 +18,44 @@ namespace Oppgave10._3
                 Console.WriteLine($"Hello {name.ToUpper()}!");
             }
 
-            //for (int i = 0; i < names.Count; i++)
-            //{
-            //    Console.WriteLine($"Hello {names[i].ToUpper()}!");
-            //}
+            for (int i = 0; i < names.Count; i++)
+            {
+                Console.WriteLine($"Hello {names[i].ToUpper()}!");
+            }
 
-            var index = names.IndexOf("Agith"); 
-            Console.WriteLine($"Fant Agith i Index {index}");
+            var index = names.IndexOf("Agith");
+            if (index == -1)
+            {
+                Console.WriteLine("fant ikke index av Agith");
+            }
+            else
+            {
+                Console.WriteLine($"Fant Agith i Index {index}");
+            }
+
+            names.Sort();
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            //----------------------------------------------------------
+
+            var fibonacciNumbers = new List<int> {1, 1};
+
+            while (fibonacciNumbers.Count < 20)
+            {
+                var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+                var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+                fibonacciNumbers.Add(previous + previous2);
+            }
+
+            foreach (var item in fibonacciNumbers)
+            {
+                Console.WriteLine(item);
+            }
+
         }
     }
 }
