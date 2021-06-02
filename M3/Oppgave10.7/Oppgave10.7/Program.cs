2d4037e7d0da525c8b038b96d654f9641a6db0ff
Agith-Ravn <76996657+Agith-Ravn@users.noreply.github.com>
2021-06-02 09:11:53 +0200

    .

---
+++ M3/Oppgave10.7/Oppgave10.7/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Oppgave10._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
