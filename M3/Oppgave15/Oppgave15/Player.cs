b42ca2a8363972449f2c8e1425378967eaa888c6
Agith-Ravn <76996657+Agith-Ravn@users.noreply.github.com>
2021-06-10 00:01:30 +0200

    .

---
+++ M3/Oppgave15/Oppgave15/Player.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Oppgave15
+{
+    public class Player
+    {
+        private int points { get; set; }
+        private string playerName { get; set; }
+
+        public Player(string playerName, int points)
+        {
+            this.playerName = playerName;
+            this.points = points;
+        }
+
+        public void Play(Player player2, Random random)
+        {
+            int playerOne = 1;
+            int playerTwo = 2;
+
+            int winner = random.Next(1, 3);
+
+            points = playerOne == winner ? points++ : points--;
+            player2.points = playerTwo == winner ? +1 : -1;
+
+            Console.WriteLine(points);
+            Console.WriteLine(player2.points);
+        }
+
+        public void ShowNameAndPoints()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ M3/Oppgave15/Oppgave15/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Oppgave15
+{
+    /* - Skal være 3 spilere
+     * - hver spiller har et navn og har 10 poeng hver.
+     * - Velger 2 tilfeldige spillere som skal spille mot hverandre
+     * - Når play() method blir kjørt, skal det velges 1 tilfeldig vinner og taper.
+     * - vinneren får 1+, taperen mister 1-
+     */
+
+    /* - Program - Game - Player
+     *  
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            var players = new[] { new Player("Per", 10), new Player("Pål", 10), new Player("Espen", 10) };
+            
+            //For loop (kjører loop 10x)
+            for (var round = 1; round <= 10; round++)
+            {
+                var playerIndex1 = random.Next(players.Length);
+                var playerIndex2 = (playerIndex1 + 1 + random.Next(2)) % players.Length;
+                var player1 = players[playerIndex1];
+                var player2 = players[playerIndex2];
+
+                // Tilfeldig player object
+                // > Kjører method som er inni object
+                // + tar med tilfeldig player 2 object 
+                player1.Play(player2, random);
+            }
+
+            //foreach (var player in players)
+            //{
+            //    player.ShowNameAndPoints();
+            //}
+
+
+        }
+    }
+}
