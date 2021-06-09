32902f607956bcd45dbd1dd55f6609e2c4c9c0c1
Agith-Ravn <76996657+Agith-Ravn@users.noreply.github.com>
2021-06-09 14:06:26 +0200

    .

---
+++ M3/Oblig2/Oblig2/Bishop.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Oblig2
+{
+    public class Bishop : Piece
+    {
+        //Base = sender informasjon fra parameter til symbol i Piece class (class som blir arvet)
+        public Bishop(string symbol) : base(symbol)
+        {
+        }
+
+        public override bool Move(string fromPosition, string toPosition)
+        {
+            var diffCol = fromPosition[0] - toPosition[0];
+            var diffRow = fromPosition[1] - toPosition[1];
+            return Math.Abs(diffRow) == Math.Abs(diffCol);
+        }
+    }
+}
+
+//public bool Move(string fromPosition, string toPosition)
+//{
+//    if (_type == "Rook") return fromPosition[0] == toPosition[0] || fromPosition[1] == toPosition[1];
+//    if (_type == "Bishop")
+//    {
+//        var diffCol = fromPosition[0] - toPosition[0];
+//        var diffRow = fromPosition[1] - toPosition[1];
+//        return Math.Abs(diffRow) == Math.Abs(diffCol);
+//    }
+//    return true;
+//}
