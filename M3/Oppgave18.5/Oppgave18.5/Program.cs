feb5be57811a26f86ab0ddf014c4f2615d2153ab
Agith-Ravn <76996657+Agith-Ravn@users.noreply.github.com>
2021-07-05 21:32:01 +0200

    .

--- M3/Oppgave18.5/Oppgave18.5/Program.cs
+++ M3/Oppgave18.5/Oppgave18.5/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Oppgave18._5
 {
@@ -6,7 +8,17 @@ namespace Oppgave18._5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var list = new List<Regisration>();
+
+            using (var file = new StreamReader("startlist.csv"))
+            {
+                string line;
+                while ((line = file.ReadLine()) != null)
+                {
+                    var registration = new Regisration(line);
+                    list.Add(registration);
+                }
+            }
         }
     }
 }
---
+++ M3/Oppgave18.5/Oppgave18.5/Registration.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Oppgave18._5
+{
+    public class Registration
+    {
+        public int StartNumber { get; set; }
+        public string Name { get; set; }
+        public string Club { get; set; }
+        public string Nationality { get; set; }
+        public string Group { get; set; }
+        public string Class { get; set; }
+
+        public Registration(string fileLine)
+        {
+            var part = fileLine.Split(",");
+            StartNumber = Convert.ToInt32(part[0]);
+            Name = part[1];
+            Club = part[2];
+            Nationality = part[3];
+            Group = part[4];
+            Class = part[5];
+        }
+
+        //public Registration(int startNumber, string name, string club, string nationality, string @group, string @class)
+        //{
+        //    StartNumber = startNumber;
+        //    Name = name;
+        //    Club = club;
+        //    Nationality = nationality;
+        //    Group = @group;
+        //    Class = @class;
+        //}
+    }
+}
